cb302994c9128b3af8c23a02c56c42129fa8edef
ian.bready <ian.bready@kearsarge.org>
2014-12-05 17:58:25 +0000

    Added some skeleton code;it won't run yet but it's progress.

--- Bullet.py
+++ Bullet.py
@@ -1,10 +1,66 @@
+import pygame
+from Ball import Ball
 
-
-
-def main():
+class Bullet(Ball)
+	def __init__(self, pos):
+		Ball.__init__(self, "ronald1", [0,0], pos)
 	
-	return 0
-
-if __name__ == '__main__':
-	main()
+	
+	def animate(self):
+		if self.waitCount < self.maxWait:
+			self.waitCount += 1
+		else:
+			self.waitCount = 0
+			self.changed = True
+			if self.frame < self.maxFrame:
+				self.frame += 1
+			else:
+				self.frame = 0
+		
+		if self.changed:	
+			if self.facing == "up":
+				self.images = self.upImages
+			elif self.facing == "down":
+				self.images = self.downImages
+			elif self.facing == "right":
+				self.images = self.rightImages
+			elif self.facing == "left":
+				self.images = self.leftImages
+			
+			self.image = self.images[self.frame]
+			
+	def update(self, width, height):
+		Ball.update(self, width, height)
+		self.animate()
+		self.changed = False
+	
+	def go(self, direction):
+		if direction == "up":
+			self.facing = "up"
+			self.changed = True
+			self.speedy = -self.maxSpeed
+		elif direction == "stop up":
+			self.speedy = 0
+		elif direction == "down":
+			self.facing = "down"
+			self.changed = True
+			self.speedy = self.maxSpeed
+		elif direction == "stop down":
+			self.speedy = 0
+			
+		if direction == "right":
+			self.facing = "right"
+			self.changed = True
+			self.speedx = self.maxSpeed
+		elif direction == "stop right":
+			self.speedx = 0
+		elif direction == "left":
+			self.facing = "left"
+			self.changed = True
+			self.speedx = -self.maxSpeed
+		elif direction == "stop left":
+			self.speedx = 0
+	
+	def hit()
+		if 
 
