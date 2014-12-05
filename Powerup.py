ce8dd5f0ac2945bea516cc4b2bd277d9cda0f94c
ian.bready <ian.bready@kearsarge.org>
2014-12-05 18:07:46 +0000

    Starting code

--- Powerup.py
+++ Powerup.py
@@ -1,10 +1,8 @@
+import pygame
 
-
-
-def main():
-	
-	return 0
-
-if __name__ == '__main__':
-	main()
-
+class Powerup(Ball):
+	def __init__(self, pos):
+		Powerup.__init__(self, "images/Powerup/powerup1.png", [0,0], pos)
+		self.upImages = [pygame.image.load("images/Powerup/powerup2.png"),
+						 pygame.image.load("images/Powerup/powerup3.png"),
+						 pygame.image.load("images/Powerup/powerup4.png")]                                                                                                            
