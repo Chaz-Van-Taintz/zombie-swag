ae38850c27ce81d05a771ba5d1eef15a89df80d0
Ian Breadyswag <ian.bready@kearsarge.org>
2014-12-19 13:31:54 -0500

    gun subset

---
+++ Gun.py
@@ -0,0 +1,20 @@
+import pygame
+
+class Gun():
+	def __init__(self, kind):
+		self.coolDown = 0
+		if kind == "pistol":
+			self.kind = kind
+			self.gunSpeed = 10
+			self.ammo = None
+			self.coolDownMax = 1
+		if kind == "shot gun":
+			self.kind = kind
+			self.gunSpeed = 7
+			self.ammo = None
+			self.coolDownMax = 1
+		if kind == "uzi":
+			self.kind = kind
+			self.gunSpeed = 15
+			self.ammo = None
+			self.coolDownMax = int(60*.1)
